0ecd20c02c269f555ee5938b0225d3727fe5ccf0
Nishant Sharma <goldenryu2000@gmail.com>
2021-10-03 16:01:12 +0530

    Finally did Any Base Multiplication

--- Level-1/3.FucntionsandArrays/AnyBaseMultiplication.java
+++ Level-1/3.FucntionsandArrays/AnyBaseMultiplication.java
@@ -1,45 +1,69 @@
-import java.util.Scanner;
-public class AnyBaseMultiplication{
+import java.util.*;
+
+public class AnyBaseMultiplication {
+    // Solution to Any Base multiplication problem from pepcoding Level-1
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int b = scn.nextInt();
         int n1 = scn.nextInt();
         int n2 = scn.nextInt();
 
-        System.out.println(getProduct(n1,n2,b));
+        int d = getProduct(b, n1, n2);
+        System.out.println(d);
         scn.close();
     }
 
-    public static int getProduct(int n1,int n2,int b) {
-        int pdt = 0;
-        int c = 0;
-        int tp=1; //powers of 10
-        int ttp = 1;
-        int p = 0;
-        while(c>0)
-        {
-            int d2 = n2%10;
-            n2 = n2/10;
-            int temp = n1;
-            while(temp > 0)
-            {
-                int d1 = temp%10;
-                temp = temp/10;
+    public static int getProduct(int b, int n1, int n2) {
+        // write your code here
+        int ans = 0;
+        int p = 1;
+        while (n2 > 0) {
+            int d2 = n2 % 10;
+            n2 = n2 / 10;
 
-                int d = d1*d2 + c;
-
-                c = d/10;
-                d = d%10;
-
-                p = p + d*ttp;
-                ttp*=10;
-            }
-
-            pdt = pdt + p*tp;
-            tp*=10;
+            int psd = getProductSingleDigit(b, n1, d2);
+            ans = getSum(b, ans, psd * p);
+            p *= 10;
         }
-
-        return pdt;
-        
+        return ans;
     }
+
+    public static int getProductSingleDigit(int b, int n1, int d2) {
+        int psd = 0;
+        int c = 0;
+        int p = 1;
+        while (n1 > 0 || c > 0) {
+            int d1 = n1 % 10;
+            n1 = n1 / 10;
+
+            int d = d1 * d2 + c;
+            c = d / b;
+            d = d % b;
+            psd = psd + d * p;
+            p *= 10;
+        }
+        return psd;
+
+    }
+
+    public static int getSum(int b, int n1, int n2) {
+        int sum = 0;
+        int c = 0;
+        int p = 1;
+        while (n1 > 0 || n2 > 0 || c > 0) {
+            int d1 = n1 % 10;
+            int d2 = n2 % 10;
+            n1 = n1 / 10;
+            n2 = n2 / 10;
+
+            int d = d1 + d2 + c;
+            c = d / b;
+            d = d % b;
+
+            sum = sum + d * p;
+            p *= 10;
+        }
+        return sum;
+    }
+
 }
